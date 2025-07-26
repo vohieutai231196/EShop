1b36e62397b104de63028705510765611f19eb15
Vo Tai <votai@Vos-MacBook-Pro.local>
2025-07-26 21:41:48 +0700

    Feat: base code of catalog

---
+++ EShop-Mircoservices/Catalog/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CatalogAPI.Model
+{
+	public class Product
+	{
+		public Product()
+		{
+		}
+	}
+}
+
---
+++ EShop-Mircoservices/Catalog/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+
+app.Run();
+
+
