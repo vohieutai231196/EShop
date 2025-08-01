a3d36ed049b7b201c5bbf6dc9be9e63045d6cb83
VoTai <vohieutai2311@gmail.com>
2025-08-01 10:39:52 +0700

    feat: the rest of minimal api Create Product

--- EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
+++ EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,11 +1,32 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductRequest(string Name, decimal Price, decimal? DiscountPrice, int StockQuantity, string? ImageUrl, int CategoryId, bool IsActive = true,
                 bool IsFeatured = false, double? Weight = null, string? Brand = null);
 
     public record CreateProductResponse(Guid Id);
 
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/Product", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/product/{response.Id}", response);
+            }).WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        }
     }
 }
