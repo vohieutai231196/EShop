7e1784a1f404f794b7a34cffd4e1828a13111643
Taivh3 <taivh3@fpt.com>
2025-08-01 09:56:20 +0700

    FEAT: CREATE FEATURE

--- EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductCommandHandler.cs
+++ EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,16 +1,32 @@
-﻿using CatalogAPI.Model;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Model;
 using MediatR;
+using System.Windows.Input;
 
 namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, decimal Price, decimal? DiscountPrice, int StockQuantity, string? ImageUrl, int CategoryId, bool IsActive = true, 
-                bool IsFeatured = false, double? Weight = null, string? Brand = null): IRequest<CreateProductResult>;
+                bool IsFeatured = false, double? Weight = null, string? Brand = null): ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id); // the value return
-    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand Command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //create Product entity from command object
+
+            var product = new Product
+            {
+                Name = Command.Name,
+                Price = Command.Price,
+                StockQuantity = Command.StockQuantity,
+                CategoryId = Command.CategoryId,
+                ImageUrl = Command.ImageUrl
+            };
+            // save to database
+
+            //return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+
         }
     }
 }
---
+++ EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, decimal Price, decimal? DiscountPrice, int StockQuantity, string? ImageUrl, int CategoryId, bool IsActive = true,
+                bool IsFeatured = false, double? Weight = null, string? Brand = null);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint
+    {
+    }
+}
