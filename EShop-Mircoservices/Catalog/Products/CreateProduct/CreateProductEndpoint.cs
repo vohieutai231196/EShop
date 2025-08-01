2090554abde7909cd1623e43dc2956aa0031513e
VoTai <vohieutai2311@gmail.com>
2025-08-01 10:35:28 +0700

    Feat: adding minimal API Create Product

--- EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
+++ EShop-Mircoservices/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
@@ -2,7 +2,9 @@
 {
     public record CreateProductRequest(string Name, decimal Price, decimal? DiscountPrice, int StockQuantity, string? ImageUrl, int CategoryId, bool IsActive = true,
                 bool IsFeatured = false, double? Weight = null, string? Brand = null);
+
     public record CreateProductResponse(Guid Id);
+
     public class CreateProductEndpoint
     {
     }
